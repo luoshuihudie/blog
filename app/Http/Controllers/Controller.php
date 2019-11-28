f79b712a67ff93b0dcee7d88c09dfdd1c894c8ee
junya.zhu <junya.zhu@vhall.com>
2019-11-28 16:18:53 +0800

    增加PHP PSR验证脚本

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,36 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * ajax请求成功返回
+     *
+     * @param array $data
+     * @return array
+     */
+    public function ajaxSucc($data = [], $code = 0, $msg = '')
+    {
+        return [
+            'code' => $code,
+            'data' => $data,
+            'msg' => $msg,
+        ];
+    }
+
+    /**
+     * ajax请求失败返回
+     *
+     * @param $code
+     * @param string $msg
+     * @param array $data
+     * @return array
+     */
+    public function ajaxError($code, $msg = '', $data = [])
+    {
+        return [
+            'code' => $code,
+            'msg' => $msg,
+            'data' => $data,
+        ];
+    }
 }
