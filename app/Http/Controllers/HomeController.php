1d9a4b527c014181a7723ba8b0fedb3fd3e4a811
junya.zhu <junya.zhu@vhall.com>
2019-11-29 18:57:25 +0800

    修改

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,14 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class HomeController extends Controller
 {
-    const STR_QUEUE_SYNC_SMS_LISTS= 'Queue:Sync:Sms:Lists'; //记录刷新文档映射
-    const STR_SYMBOL_SPOT= '.'; //记录刷新文档映射
+    const STR_QUEUE_SYNC_SMS_LISTS = 'Queue:Sync:Sms:Lists'; //记录刷新文档映射
+    const STR_SYMBOL_SPOT = '.'; //记录刷新文档映射
     const STR_CODE_MAPPING = [
         1 => 5276551628153579435,
         2 => 5276562898145752097,
     ];
     const DEFAULT_CALLBACK_ARRAY = [
-        'custid' =>'147',
+        'custid' => '147',
         'mobile' => '13521262613',
         'stime' => '2019-11-14 14:38:36',
         'status' => 0,
@@ -27,13 +27,13 @@ class HomeController extends Controller
     //
     public function welcome()
     {
-        $filePath = public_path() . '/import/'.iconv('UTF-8', 'GBK', '7346-1').'.xlsx';
+        $filePath = public_path() . '/import/' . iconv('UTF-8', 'GBK', '7346-1') . '.xlsx';
         $smsImport = new SmsImport();
         $smsArray = Excel::toArray($smsImport, $filePath);
         $defaultArray = self::DEFAULT_CALLBACK_ARRAY;
         $mapping = self::STR_CODE_MAPPING;
         $smsArray = array_map(function ($items) use ($defaultArray, $mapping) {
-            return array_map(function ($item) use ($defaultArray, $mapping){
+            return array_map(function ($item) use ($defaultArray, $mapping) {
                 $defaultArray['custid'] = $mapping[$item['code']];
                 $defaultArray['mobile'] = $item['phone'];
                 $defaultArray['stime'] = $item['stime'];
